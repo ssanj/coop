7940a59424dca99ee82261ca3ffce7740436c8dc
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-05-29 05:46:18 +1000

    first pass of walkdir

--- src/workflow/coop_workflow.rs
+++ src/workflow/coop_workflow.rs
@@ -1,4 +1,7 @@
-// use walkdir::WalkDir;
+use std::fs::FileType;
+
+use walkdir::WalkDir;
+
 use crate::cli::Args;
 
 pub struct CoopWorkflow {
@@ -13,6 +16,24 @@ impl CoopWorkflow {
   }
 
   pub fn run(self) {
-    println!("args: {:?}", self.args);
+    let args = self.args;
+    let source_dir = &args.source_dir;
+
+    let files_to_copy: Vec<_> =
+      WalkDir::new(source_dir)
+        .into_iter()
+        .filter_map(|de| {
+          de
+            .ok()
+            .filter(|d| {
+              // We only want files and not directories or symlinks
+              // We might want to filter out certain files like .DS_Store
+              d.file_type().is_file()
+            })
+            .map(|f| f.file_name().to_string_lossy().to_string() )
+        })
+        .collect();
+
+    println!("{:?}", files_to_copy)
   }
 }
