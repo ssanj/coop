c27e4b078f03e3f74dc247ef37e363e69c1a0baa
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-05-31 06:20:53 +1000

    add emoji goodness

--- src/monitor/progress_monitor.rs
+++ src/monitor/progress_monitor.rs
@@ -30,50 +30,50 @@ impl FileCopyProgressMonitor {
           },
 
           FileStatus::FileSizesMatch(pb) => {
-            pb.complete("verification complete")
+            pb.complete("verification complete ✅")
           },
 
           FileStatus::Failed(FailedReason::ReadFailed(reason, pb)) => {
-            pb.set_error(&format!("Read failed: {}", reason))
+            pb.set_error(&format!("❌ Read failed: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::WriteFailed(reason, pb)) => {
-            pb.set_error(&format!("Write failed: {}", reason))
+            pb.set_error(&format!("❌ Write failed: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::CouldNotGetFileSize(reason, FileType::Source, pb)) => {
             pb.set_status("calculating source file length...");
-            pb.set_error(&format!("Could not get source file size: {}", reason))
+            pb.set_error(&format!("❌ Could not get source file size: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::CouldNotGetFileSize(reason, FileType::Destination, pb)) => {
             pb.set_status("calculating destination file length...");
-            pb.set_error(&format!("Could not get destination file size: {}", reason))
+            pb.set_error(&format!("❌ Could not get destination file size: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::CouldNotCreateDestinationFile(reason, pb)) => {
             pb.set_status("creating destination file...");
-            pb.set_error(&format!("Could not create destination file: {}", reason))
+            pb.set_error(&format!("❌ Could not create destination file: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::CouldNotCreateDestinationDir(reason, pb)) => {
             pb.set_status("creating destination dir...");
-            pb.set_error(&format!("Could not create destination dir: {}", reason))
+            pb.set_error(&format!("❌ Could not create destination dir: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::CouldNotReadSourceFile(reason, pb)) => {
             pb.set_status("opening source file...");
-            pb.set_error(&format!("Could not read source file: {}", reason))
+            pb.set_error(&format!("❌ Could not read source file: {}", reason))
           },
 
           FileStatus::Failed(FailedReason::FileSizesAreDifferent(source, dest, pb)) => {
             pb.set_status("comparing source and destination file sizes...");
-            pb.set_error(&format!("File sizes are different. src:{source}, dst:{dest}"))
+            pb.set_error(&format!("❌ File sizes are different. src:{source}, dst:{dest}"))
           },
 
           FileStatus::Failed(FailedReason::FlushFailed(reason, pb)) => {
             pb.set_status("flushing destination to disk...");
-            pb.set_error(&format!("Flushing destination file failed: {}", reason))
+            pb.set_error(&format!("❌ Flushing destination file failed: {}", reason))
           },
         }
       }
